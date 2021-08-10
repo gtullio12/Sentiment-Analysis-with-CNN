6333483299bad3c93f09a5ad27cdfa70d6c4c338
gtullio12 <gtullio12@protonmail.com>
2021-08-09 20:57:15 -0700

    Add tokenizer implementation

--- twitter.py
+++ twitter.py
@@ -1,3 +1,11 @@
+from keras.preprocessing.text import Tokenizer
+
+# Fit a tokenizer
+def create_tokenizer(lines):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
 def load_doc(filename):
     doc = open(filename, 'r')
     text = doc.read()
@@ -7,4 +15,12 @@ def load_doc(filename):
 vocab_filename = 'vocab.txt'
 doc = load_doc(vocab_filename)
 vocab = doc.split()
-print(vocab)
+# Get list of cleaned tweets
+data_filename = 'reviews.txt'
+doc = load_doc(data_filename)
+tweets = doc.split('\n')
+
+# Create tokenizer
+tokenizer = create_tokenizer(tweets)
+# Encode data
+Xtrain = tokenizer.texts_to_matrix(tweets, mode='binary')
