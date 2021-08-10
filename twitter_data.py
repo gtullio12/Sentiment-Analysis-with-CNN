3f9048aa2ded1b9d24c563e9d049aa75bfe9f17a
gtullio12 <gtullio12@protonmail.com>
2021-08-09 21:28:12 -0700

    initial commit

--- twitter_data.py
+++ twitter_data.py
@@ -1,15 +1,18 @@
 import tweepy as tw
+from nltk.corpus import stopwords
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from collections import Counter
 import re
 import string
-from nltk.corpus import stopwords
-import nltk
 nltk.download('stopwords')
+nltk.download('vader_lexicon')
 
 consumer_key = 'KnUlN9kEQ7gJdDiVp8oNE1Wg2'
 consumer_secret = 'mRtOVvdiZIzqwdxebqo47WmOBGBvWjIwtZvufDU2E84vlm5QV4'
 access_token = '1027293645676769280-2heVzKV4eDN7LDv0Udtk8VkE8Ubj9m'
 access_token_secret = '6RdwPKQbgIX75EcsI3eovdsqhtomqUgsHWo4Lwa1lOwnM'
+sid = SentimentIntensityAnalyzer()
 
 def clean_tweets(tweets):
     tokens = tweets.split()
@@ -29,6 +32,20 @@ def clean_tweets(tweets):
     tokens = [word for word in tokens if word != 'rt' and 'http' not in word]
     return tokens
 
+# -1 = Negative tweet. 1 = Positive tweet. 0 = Neutral tweet
+def classify_tweet(tweet):
+    ss = sid.polarity_scores(tweet)
+    neg_score = ss['neg']
+    pos_score = ss['pos']
+    neu_score = ss['neu']
+    if neg_score > pos_score and neg_score > neu_score:
+        return -1
+    elif pos_score > neg_score and pos_score > neu_score:
+        return 1
+    else:
+        return 0
+    return 1
+
 # Create a file with one cleaned tweet on each line.
 def add_tweets_to_file(tweets, filename):
     # Remove newlines in each tweet
@@ -36,9 +53,12 @@ def add_tweets_to_file(tweets, filename):
     # Clean each tweet
     cleaned_tweets = [clean_tweets(tweet) for tweet in tweets]
     # Combine all tweets to a document, where each tweet is seperated by newline
-    reviews = [' '.join(tweet) for tweet in cleaned_tweets]
-    print('Number of tweets: %d ' %len(reviews))
-    save_list(reviews, filename)
+    tokens = [' '.join(tweet) for tweet in cleaned_tweets]
+    # Classify tweet as positive or negative 
+    classifications = [classify_tweet(token) for token in tokens]
+    classifications = [str(classify) for classify in classifications] 
+    save_list(classifications, 'classify.txt')
+    save_list(tokens, filename)
     return None
 
 def save_list(lines, filename):
@@ -52,16 +72,15 @@ def add_tweets_to_vocab(tweets, vocab):
     tokens = clean_tweets(words)
     vocab.update(tokens)
 
-
 # Authenticate with Twitter
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
 # Define the search term and the date_since date as variables
-search_words = "#crypto -filter:retweets"
+search_words = "#Gamestop OR #gamestop OR #gme AND up OR down -filter:retweets"
 date_since = "2018-11-16"
-num_of_tweets = 100
+num_of_tweets = 1000
 
 # Collect tweets
 tweets = tw.Cursor(api.search,
